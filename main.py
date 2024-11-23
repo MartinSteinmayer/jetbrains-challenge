1972f0d2f7ee837215424e119249368e395fcdcf
Martin Steinmayer <martin.j.steinmayer@gmail.com>
2024-11-23 16:26:50 +0100

    first graph

---
+++ main.py
@@ -0,0 +1,200 @@
+"""
+Main implementation of the multi-agent copilot.
+"""
+
+# General imports
+from re import L
+from typing import Annotated, Literal, TypedDict, Sequence, Callable
+import asyncio
+import os
+from langchain_core import messages
+from pydantic import BaseModel, Field
+import operator
+import functools
+from dotenv import load_dotenv
+
+# Langchain
+from langchain_core.messages import AIMessage, HumanMessage, BaseMessage
+from langchain_core.tools import tool
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.output_parsers import StrOutputParser
+
+# Langgraph
+from langgraph.graph import END, START, StateGraph
+from langgraph.prebuilt import ToolNode
+
+chat_history = []
+
+
+# Defining State
+class AgentState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], operator.add]
+    next: Literal["Checker", "Optimizer", "Sanitizer", "Helper", "END"]
+    sender: Literal["Checker", "Optimizer", "Sanitizer", "Helper", "END"]
+
+
+# Creating the agent -> Basically a model that can call tools and gives output in a specific manner
+def create_agent(tools: list, system_prompt: str):
+
+    model = ChatOpenAI(model="gpt-4o", api_key=os.getenv("OPENAI_API_KEY"), temperature=0.15, max_tokens=4000)
+
+    if len(tools) > 0:
+        model = model.bind_tools(tools, tool_choice="auto")
+
+    prompt = ChatPromptTemplate.from_messages([("system", "{system_prompt}"),
+                                               MessagesPlaceholder(variable_name="messages")
+                                              ]).partial(system_prompt=system_prompt)
+
+    # Agent is a chain of the prompt and the model
+    agent = (prompt | model)
+
+    return agent
+
+
+# Defining the agent node -> Basically to define a node you have to have a function with a state parameter and
+async def agent_node(state, agent, name):
+
+    result = await agent.ainvoke(state)
+    print(result)
+
+    if result.tool_calls:
+        pass
+    else:
+        result = AIMessage(content=result.content, name=name)
+
+    return {"messages": [result], "sender": name}
+
+
+# Define the tools for the agent to use
+@tool
+def search(query: str):
+    """Call to surf the web."""
+    # This is a placeholder, but don't tell the LLM that...
+    if "sf" in query.lower() or "san francisco" in query.lower():
+        return "It's 60 degrees and foggy."
+    return "It's 90 degrees and sunny."
+
+
+async def main():
+
+    input_text = input("Enter the text: ")
+
+    # Load environment variables
+    load_dotenv()
+
+    print(os.getenv("OPENAI_API_KEY"))
+
+    # Define the model for the supervisor
+    model = ChatOpenAI(model="gpt-4o",
+                       api_key=os.getenv("OPENAI_API_KEY"),
+                       max_retries=1,
+                       max_tokens=5000,
+                       temperature=0.25)
+
+    # Define worker nodes
+    workers = ["Linter", "Optimizer", "Sanitizer", "Helper"]
+
+    supervisor_system_prompt = (
+        "You are a supervisor in the context of a multi-agent coding copilot for Jetbrains IDEs."
+        "You are responsible for overseeing the work of the following agents: {workers}"
+        "Given the following user request, respond with the worker to act next. Each worker will perform a task and respond with their results and status. When finished, respond with FINISH."
+    )
+
+    class routeResponse(BaseModel):
+        next: Literal["Linter", "Optimizer", "Sanitizer", "Helper"]
+
+    supervisorPrompt = ChatPromptTemplate.from_messages([
+        ("system", supervisor_system_prompt),
+        MessagesPlaceholder(variable_name="messages"),
+        ("system", """ Given the conversation above, who should act next? Keep the following in mind: /
+                The sanitizer is supposed to find errors in the code. It will compile the code and run code sanitizers such as Valgrind.
+                The optimizer is a general solution that should be used when the user wants to generate code, optimize an existing piece of code or wants an explanation for a piece of code.
+                The linter is going to run linters in the background to find style errors. 
+                The helper is supposed to answer questions about Jetbrains or general questions the user might ask that are unrelated to code.
+                Select one of: {workers}
+                """)
+    ]).partial(workers=str(workers))
+
+    # Supervisor is a node that routes the conversation to the next worker
+    async def supervisor_agent(state):
+        supervisor_chain = supervisorPrompt | model.with_structured_output(routeResponse)
+        response = await supervisor_chain.ainvoke(state)
+        print(response)
+        return response
+
+    ### Creating Agents -> Using functools.partial to set agent and name parameters ###
+    linter_prompt = "You are a linter agent. You are responsible for running linters in the background to find style errors."
+    linter_agent = functools.partial(agent_node, agent=create_agent([], linter_prompt), name="Linter")
+
+    optimizer_prompt = "You are an optimizer agent. You are a general solution that should be used when the user wants to generate code, optimize an existing piece of code or wants an explanation for a piece of code."
+    optimizer_agent = functools.partial(agent_node, agent=create_agent([], optimizer_prompt), name="Optimizer")
+
+    sanitizer_prompt = "You are a sanitizer agent. You are responsible for finding errors in the code. You will compile the code and run code sanitizers such as Valgrind."
+    sanitizer_agent = functools.partial(agent_node, agent=create_agent([], sanitizer_prompt), name="Sanitizer")
+
+    helper_prompt = "You are a helper agent. You are responsible for answering questions about Jetbrains or general questions the user might ask that are unrelated to code."
+    helper_agent = functools.partial(agent_node, agent=create_agent([], helper_prompt), name="Helper")
+
+    ### Define the StateGraph ###
+    graph = StateGraph(AgentState)
+    graph.add_node("Supervisor", supervisor_agent)
+    graph.add_node("Linter", linter_agent)
+    graph.add_node("Optimizer", optimizer_agent)
+    graph.add_node("Sanitizer", sanitizer_agent)
+    graph.add_node("Helper", helper_agent)
+
+    # Define the function that determines which agent to route to
+    def choose_agent(state) -> Literal["Linter", "Optimizer", "Sanitizer", "Helper"]:
+        return state["next"]
+
+    # Maps each node name to the corresponding agent
+    conditional_map = {k: k for k in workers}
+    graph.add_conditional_edges("Supervisor", choose_agent, conditional_map)
+
+    # Setup Tools
+    sanitizer_tools = ToolNode([search])
+    optimizer_tools = ToolNode([search])
+    linter_tools = ToolNode([search])
+    helper_tools = ToolNode([search])
+
+    graph.add_node("SanitizerTools", sanitizer_tools)
+    graph.add_node("OptimizerTools", optimizer_tools)
+    graph.add_node("LinterTools", linter_tools)
+    graph.add_node("HelperTools", helper_tools)
+
+    def router_tools(state):
+        messages = state['messages']
+        last_message = messages[-1]
+
+        if last_message.tool_calls:
+            return state["next"] + "Tools"
+
+        return "END"
+
+    graph.add_conditional_edges("Sanitizer", router_tools, {"SanitizerTools": "SanitizerTools", "END": END})
+    graph.add_conditional_edges("Optimizer", router_tools, {"OptimizerTools": "OptimizerTools", "END": END})
+    graph.add_conditional_edges("Linter", router_tools, {"LinterTools": "LinterTools", "END": END})
+    graph.add_conditional_edges("Helper", router_tools, {"HelperTools": "HelperTools", "END": END})
+
+    graph.add_edge("SanitizerTools", "Sanitizer")
+    graph.add_edge("OptimizerTools", "Optimizer")
+    graph.add_edge("LinterTools", "Linter")
+    graph.add_edge("HelperTools", "Helper")
+
+    graph.add_edge(START, "Supervisor")
+
+    workflow = graph.compile()
+
+    # Call the workflow
+    response = await workflow.ainvoke({"messages": [HumanMessage(content=input_text)]})
+
+    response_text = response["messages"][-1].content
+    print(response_text)
+
+    return response_text
+
+
+# Run main with asyncio
+if __name__ == "__main__":
+    asyncio.run(main())
